a594576db5992c24b9c8fdc3e799ffc19f72248a
Mitchell Berg <mb@nowdiscover.com>
2017-01-23 17:12:03 +0100

    added in a default source for testing

--- src/main/java/com/verzano/terminalrss/TerminalRSS.java
+++ src/main/java/com/verzano/terminalrss/TerminalRSS.java
@@ -1,8 +1,14 @@
 package com.verzano.terminalrss;
 
+import com.sun.syndication.feed.synd.SyndEntryImpl;
+import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 import com.verzano.terminalrss.article.Article;
 import com.verzano.terminalrss.article.ArticleManager;
+import com.verzano.terminalrss.exception.ArticleExistsException;
+import com.verzano.terminalrss.exception.SourceExistsException;
 import com.verzano.terminalrss.source.Source;
 import com.verzano.terminalrss.source.SourceManager;
 import com.verzano.terminalrss.ui.TerminalUI;
@@ -10,10 +16,14 @@ import com.verzano.terminalrss.ui.widget.bar.BarWidget;
 import com.verzano.terminalrss.ui.widget.constants.Direction;
 import com.verzano.terminalrss.ui.widget.list.ListWidget;
 import com.verzano.terminalrss.ui.widget.text.TextAreaWidget;
+import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 import static com.verzano.terminalrss.ui.widget.action.Key.DELETE;
 import static com.verzano.terminalrss.ui.widget.action.Key.ENTER;
@@ -31,6 +41,11 @@ public class TerminalRSS {
   private static TextAreaWidget contentTextAreaWidget;
 
   public static void main(String[] args) throws IOException, FeedException {
+    Collection<Source> sources = SourceManager.getSources();
+    if (sources.isEmpty()) {
+      addSource("http://www.theverge.com/rss/index.xml", "c-entry-content");
+    }
+
     buildSourceWidgets();
     buildArticleWidgets();
     buildContentTextAreaWidget();
@@ -115,5 +130,48 @@ public class TerminalRSS {
       articlesListWidget.setFocused();
       TerminalUI.reprint();
     });
+  }
+
+  private static void addSource(String uri, String contentTag) {
+    try {
+      SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(uri)));
+      Long id = SourceManager.createSource(uri, contentTag, feed.getPublishedDate(), feed.getTitle());
+      updateSource(id);
+    } catch (FeedException | IOException e) {
+      // TODO logging
+      throw new RuntimeException(e);
+    } catch (SourceExistsException ignored) {
+      // TODO notify the user
+    }
+  }
+
+  private static void updateSource(Long sourceId) {
+    Source source = SourceManager.getSource(sourceId);
+    if (source == Source.NULL_SOURCE) {
+      System.err.println("No source for source_id: " + sourceId);
+    } else {
+      try {
+        SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(source.getUri())));
+
+        ((List<SyndEntryImpl>) feed.getEntries()).forEach(entry -> {
+          try {
+            ArticleManager.createArticle(
+                sourceId,
+                entry.getUri(),
+                entry.getPublishedDate(),
+                entry.getTitle(),
+                Jsoup.connect(entry.getUri()).get().getElementsByClass(source.getContentTag()).get(0).text(),
+                entry.getUpdatedDate());
+          } catch (IOException e) {
+            // TODO logging
+            throw new RuntimeException(e);
+          } catch (ArticleExistsException ignored) {
+            // TODO notify the user
+          }
+        });
+      } catch(FeedException | IOException e){
+        System.err.println("Failed to update source");
+      }
+    }
   }
 }
